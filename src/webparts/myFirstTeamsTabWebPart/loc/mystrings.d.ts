1628cdafcc80339029e7e0d96ddf1eaa870ad0b2
Tony Minseok Kim <tony.minseokkim@tectiqom.de>
2022-02-18 15:28:16 -0800

    .

---
+++ src/webparts/myFirstTeamsTabWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMyFirstTeamsTabWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MyFirstTeamsTabWebPartWebPartStrings' {
+  const strings: IMyFirstTeamsTabWebPartWebPartStrings;
+  export = strings;
+}
